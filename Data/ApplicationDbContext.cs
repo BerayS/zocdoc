4e0ffc6923d3c2d7f38631f350228096a10e8ceb
Beray <bsezgin00@gmail.com>
2023-01-24 11:18:02 +0200

    Added Calendars, Data Models and Database Files

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using AspNetCoreCookieAuthentication.Data;
 
 namespace AspNetCoreCookieAuthentication.Data {
     public class ApplicationDbContext : DbContext {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        public DbSet<AppointmentSlot> Appointments { get; set; }
         public DbSet<User> Users {get;set;}
     }
 }
---
+++ Data/Data.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNetCoreCookieAuthentication.Data {
+    public class AppointmentSlot {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public string? PatientName { set; get; }
+
+        public string? Text => PatientName;
+
+        [JsonPropertyName("patient")]
+        public string? PatientId { set; get; }
+
+        public string Status { get; set; } = "free";
+    }
+}
